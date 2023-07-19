59aeaa8eded0a64ed80da749314965aafb813ab7
Guiodani <axel_guiodane@hotmail.com>
2023-07-19 16:31:39 -0400

    correction

--- .vscode/assets/script.js
+++ .vscode/assets/script.js
@@ -7,10 +7,10 @@ const img = document.querySelector ('#profile img')
     if (html.classList.contains("light")) {
 
        
-     img.setAttribute("src", "./avatar-light.jpg")
+     img.setAttribute("src", "./assets/avatar-light.jpg")
 } else {
 
-    img.setAttribute("src", "./avatar.jpg")
+    img.setAttribute("src", "./assets/avatar.jpg")
 
 }
 }
